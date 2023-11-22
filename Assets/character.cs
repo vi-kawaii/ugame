86c65ed34179c2272c2359fa2d8855d11fb33de1
vi-wivi <velianjanjan@gmail.com>
2023-11-22 23:03:38 +0500

    камера и движение

--- Assets/character.cs
+++ Assets/character.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Example : MonoBehaviour
+public class character : MonoBehaviour
 {
     public CharacterController controller;
+    public Camera camera;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {
@@ -19,16 +24,15 @@ public class Example : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        Vector3 moveX = camera.transform.right * Input.GetAxis("Horizontal");
+        Vector3 moveZ = Quaternion.AngleAxis(-90, Vector3.up) * camera.transform.right * Input.GetAxis("Vertical");
+        
+        var moveDirection = moveX + moveZ;
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        controller.Move(moveDirection * Time.deltaTime * playerSpeed);
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (Input.GetAxisRaw("Jump") == 1 && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
