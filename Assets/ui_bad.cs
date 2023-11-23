92afd123aff763e3d4e79429121f6fa8ae07257a
vi-wivi <velianjanjan@gmail.com>
2023-11-23 18:02:41 +0500

    все еще пытаюсь

--- Assets/ui_bad.cs
+++ Assets/ui_bad.cs
@@ -14,7 +14,24 @@ public class ui_bad : MonoBehaviour
     {
         var leftBottomPoint = camera.WorldToScreenPoint(enemy.transform.position);
         var centerPoint = leftBottomPoint - new Vector3(Screen.width / 2, Screen.height / 2, 0);
-        rt.anchoredPosition = centerPoint;
-        // rt.anchoredPosition = camera.WorldToScreenPoint(targPos);
+        var pointInsideScreen = new Vector3(
+            Mathf.Clamp(centerPoint.x, 0 - Screen.width / 2, Screen.width / 2),
+            Mathf.Clamp(centerPoint.y, 0 - Screen.height / 2, Screen.height / 2),
+            centerPoint.z
+        );
+
+        if (pointInsideScreen.z < 0)
+        {
+            pointInsideScreen = new Vector3(
+                pointInsideScreen.x < 0 ? Screen.width / 2 : 0 - Screen.width / 2,
+                pointInsideScreen.y < 0 ? 0 - Screen.height / 2 : Screen.height / 2,
+                // Mathf.Clamp(pointInsideScreen.y - Screen.height, 0 - Screen.height / 2, Screen.height / 2),
+                pointInsideScreen.z
+            );
+        }
+
+        Debug.Log(pointInsideScreen);
+
+        rt.anchoredPosition = pointInsideScreen;
     }
 }
