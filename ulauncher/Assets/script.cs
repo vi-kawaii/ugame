bb3f9e3a51d2db9941b4a65997dc0155355c5dc6
vi-wivi <velianjanjan@gmail.com>
2023-12-16 23:42:03 +0500

    добавил запуск процесса

--- ulauncher/Assets/script.cs
+++ ulauncher/Assets/script.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
+    void Start()
+    {
+        System.Diagnostics.Process.Start("explorer");
+    }
+
     void Update()
     {
         var x = System.Diagnostics.Process.GetProcessesByName("notepad");
