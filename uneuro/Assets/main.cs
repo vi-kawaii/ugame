068ba49562f92253961e2ecf3c0acb110bd4f507
vi-wivi <velianjanjan@gmail.com>
2024-01-28 22:29:13 +0500

    попытка написать рабочий код

---
+++ uneuro/Assets/main.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using SentimentAnalysis;
+
+public class main : MonoBehaviour
+{
+    bool flag = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag)
+        {
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = "This restaurant was wonderful."
+            };
+
+            Debug.Log($"Text: {sampleData.Col0}\nSentiment: {SentimentModel.Predict(sampleData).PredictedLabel}");
+            flag = false;
+        }
+    }
+}
